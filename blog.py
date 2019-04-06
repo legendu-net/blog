5fa309853727466ed7b491cab4fa80416fdb4c66
Ben Chuanlong Du <dclong@users.noreply.github.com>
2019-04-05 22:24:23 -0700

    Update blog.py

--- blog.py
+++ blog.py
@@ -283,8 +283,7 @@ class Blogger:
         content = ''.join(line.strip() for line in lines)
         empty = re.sub(r'\*\*.+\*\*', '', content) == ''
         return 1 if empty else 0
-            
-        
+  
     def delete(self, posts: Union[str, List[str]]):
         if isinstance(posts, str):
             posts = [posts]

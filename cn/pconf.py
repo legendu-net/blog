f3197a233c7a89441283568f663e03af9e93f435
Benjamin Du <longendu@yahoo.com>
2019-10-14 23:34:50 -0700

    update linkedin link

--- cn/pconf.py
+++ cn/pconf.py
@@ -62,9 +62,11 @@ GITHUB_SHOW_USER_LINK = True
 #          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('LinkedIn', 'http://www.linkedin.com/pub/chuanlong-ben-du/21/9b2/123/'),
-            ('Twitter', 'https://twitter.com/longendu'),
-            ('Facebook', 'https://www.facebook.com/chuanlong.du'))
+SOCIAL = (
+    ('LinkedIn', 'https://www.linkedin.com/in/ben-chuanlong-du-1239b221/'),
+    ('Twitter', 'https://twitter.com/longendu'),
+    ('Facebook', 'https://www.facebook.com/chuanlong.du'),
+)
 
 DEFAULT_PAGINATION = 6
 

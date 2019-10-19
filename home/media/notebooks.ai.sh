480ed111f10fbe39c620ff0ef2b88e4bfa01ed28
legendu <longendu@yahoo.com>
2019-10-19 02:11:42 +0000

    ...

--- home/media/notebooks.ai.sh
+++ home/media/notebooks.ai.sh
@@ -5,19 +5,34 @@ mkdir -p /app/pkgs
 apt-get update -y
 apt-get install -y wajig git
 pip3 install pelican loguru
-mkdir -p archives
+mkdir -p /app/archives
 ln -svf /app/archives /root/
+
 # blog
-if [[ ! -e archives/blog ]]; then
-    git clone git@github.com:dclong/blog.git archives/
+if [[ ! -e /app/archives/blog ]]; then
+    git clone git@github.com:dclong/blog.git /app/archives/
 fi
-git -C archives/blog submodule init
-git -C archives/blog submodule update --recursive --remote
+git -C /app/archives/blog pull origin master
+git -C /app/archives/blog submodule init
+git -C /app/archives/blog submodule update --recursive --remote
+
 # config
-if [[ ! -e archives/config ]]; then
-    git clone git@github.com:dclong/config.git archives/
+if [[ ! -e /app/archives/config ]]; then
+    git clone git@github.com:dclong/config.git /app/archives/
 fi
-python3 archives/config/linstall.py poetry -ic
-python3 archives/config/linstall.py xonsh -ic
-python3 archives/config/linstall.py bash_it -ic
-python3 archives/config/linstall.py svim -ic
+git -C /app/archives/config pull origin master
+python3 /app/archives/config/linstall.py poetry -ic
+python3 /app/archives/config/linstall.py xonsh -ic
+python3 /app/archives/config/linstall.py bash_it -ic
+python3 /app/archives/config/linstall.py svim -ic --disable-true-colors
+
+# dsutil
+if [[ ! -e /app/archives/dsutil ]]; then
+    git clone git@bitbucket.org:dclong/dsutil.git /app/archives/
+fi
+git -C /app/archives/dsutil pull origin dev
+cd /app/archives/dsutil/
+poetry env use python3
+poetry build
+pip3 install dist/dsutil*.whl
+

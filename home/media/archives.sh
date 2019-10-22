3ef9f6272cf8aa77d79b12d05405acb4705dfa6a
legendu <longendu@yahoo.com>
2019-10-22 21:58:06 +0000

    auto yes

--- home/media/archives.sh
+++ home/media/archives.sh
@@ -14,7 +14,7 @@ git clone git@github.com:dclong/blog.git $HOME/archives/blog
 git clone git@github.com:dclong/config.git $HOME/archives/config
 git clone git@bitbucket.org:dclong/dsutil.git $HOME/archives/dsutil
 
-$HOME/archives/config/linstall.py sshc -ic
-$HOME/archives/config/linstall.py nodejs -ic
-$HOME/archives/config/linstall.py ipy3 -ic
-$HOME/archives/config/linstall.py svim -ic
+$HOME/archives/config/linstall.py sshc -y -ic
+$HOME/archives/config/linstall.py nodejs -y -ic
+$HOME/archives/config/linstall.py ipy3 -y -ic
+$HOME/archives/config/linstall.py svim -y -ic

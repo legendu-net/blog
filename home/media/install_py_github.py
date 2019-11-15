6908fdb165dbbca1b9c420850a05a928be590ee6
Ben Chuanlong Du <legendu.net@outlook.com>
2019-11-15 00:21:21 +0000

    add 2 more options: --sudo and --sys

--- home/media/install_py_github.py
+++ home/media/install_py_github.py
@@ -11,11 +11,11 @@ def _version(url) -> str:
     req = urllib.request.urlopen(url)
     return Path(req.url).name
 
-def install(url: str, yes: bool = False) -> None:
+def install(url: str, sudo: bool = False, sys: bool = False, yes: bool = False) -> None:
     version = _version(url)
     url = f"{url}/releases/download/{version}/{Path(url).name}-{re.sub('[a-zA-Z]', '', version)}-py3-none-any.whl"
     yes = '-y' if yes else ''
-    cmd = f'pip3 install --user --upgrade {yes} {url}'
+    cmd = f"{'sudo' if sudo else ''} pip3 install {'' if sys else '--user'} --upgrade {yes} {url}"
     sp.run(cmd, shell=True, check=True)
 
 
@@ -24,6 +24,18 @@ def parse_args(args=None, namespace=None):
     """
     parser = ArgumentParser(
         description='Automatically install the latest version of Python package from its GitHub project.'
+    )
+    parser.add_argument(
+        '--sudo',
+        dest='sudo',
+        action='store_true',
+        help="Install using sudo."
+    )
+    parser.add_argument(
+        '--sys',
+        dest='sys',
+        action='store_true',
+        help="Install to system-wide location."
     )
     parser.add_argument(
         '-y',
@@ -41,7 +53,7 @@ def parse_args(args=None, namespace=None):
 
 def main():
     args = parse_args()
-    install(args.url, args.yes)
+    install(url=args.url, sudo=args.sudo, sys=args.sys, yes=args.yes)
 
 
 if __name__ == '__main__':

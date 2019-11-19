a4c1155656faaa8c4f9351f805bb5bcda0b9c84a
Ben Chuanlong Du <legendu.net@outlook.com>
2019-11-19 11:35:54 -0800

    ...

--- home/media/config_docker.sh
+++ home/media/config_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp -r /home_host/$(id -un)/.ssh $HOME/
 ln -svf /home_host/$(id -un)/archives $HOME/
 ln -svfT /workdir/user/$(id -un) $HOME/projects
 
+xinstall sshc -c
 xinstall --sudo git -ic
 xinstall svim -ic
 xinstall ipy3 -c

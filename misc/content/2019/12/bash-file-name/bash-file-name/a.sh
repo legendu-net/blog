892af694bdbff9bb126f639c0993c7b5261b587e
Benjamin Du <longendu@yahoo.com>
2022-07-06 21:42:48 -0700

    ...

--- misc/content/2019/12/bash-file-name/bash-file-name/a.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo '$0: '$0
-echo '$(readlink -f $0): '$(readlink -f $0)
-echo '${BASH_SOURCE[0]}: '${BASH_SOURCE[0]}
-echo ' $(readlink -f ${BASH_SOURCE[0]}): '$(readlink -f ${BASH_SOURCE[0]})
--- misc/content/2019/12/bash-file-name/bash-file-name/c.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source bash-file-name/a.sh
-bash-file-name/a.sh
-echo ${BASH_SOURCE[0]}

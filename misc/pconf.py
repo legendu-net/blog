d055b2979511b02bf8d0dfbe349c834e6712e1c1
Ben Du <longendu@yahoo.com>
2024-02-13 00:08:58 -0800

    ...

--- misc/pconf.py
+++ misc/pconf.py
@@ -39,7 +39,7 @@ MENUITEMS = [
     ("Home", HOME_URL),
     ("Blog", SITEURL),
     ("Archives", f"{SITEURL}/archives.html"),
-    ("About", f"{SITEURL}/pages/about"),
+    ("About", f"{HOME_URL}/pages/about"),
 ]
 NEWEST_FIRST_ARCHIVES = True
 ARTICLE_ORDER_BY = "reversed-modified"

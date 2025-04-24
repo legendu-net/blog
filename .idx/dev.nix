6198bfcbffa681c2471a8aa818e51c35dc7fb2d5
longendu <longendu@gmail.com>
2025-04-24 05:55:39 +0000

    update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,8 +10,6 @@
     ripgrep
     rm-improved
     bat
-    python311
-    python311Packages.pip
     poetry
   ];
 
@@ -30,16 +28,6 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
       };
     };
 
@@ -47,13 +35,9 @@
     workspace = {
       # Runs when a workspace is first created
       onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
       };
       # Runs when the workspace is (re)started
       onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
         poetry-project = ''
         poetry config --local virtualenvs.in-project true
         poetry install
